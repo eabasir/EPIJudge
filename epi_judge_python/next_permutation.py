39a088ffbbaf23647447813d8ac98e999adc835e
eabasir <eabasir@gmail.com>
2022-08-20 17:31:28 +0200

    next permutation

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    if(len(perm) < 2):
+        return []
+
+    candidate = len(perm) - 1
+
+    while candidate >= 0:
+        candidate -= 1
+        if (perm[candidate] < perm[candidate + 1]):
+            break
+
+    if(candidate < 0):
+        return []
+    
+    swappable_index = candidate + 1
+    for i in range(candidate + 1, len(perm)):
+        if (perm[candidate] >= perm[i]):
+            break
+        swappable_index = i
+    
+    perm[candidate], perm[swappable_index] = perm[swappable_index], perm[candidate]
+
+    return perm[:candidate + 1] + perm[len(perm):candidate:-1]
 
 
 if __name__ == '__main__':
